9714f44bb32ecbcc45df1d7d43f883c2fc005b0b
Okuda-Haruto <k024g0057@g.neec.ac.jp>
2025-04-16 02:28:14 +0900

    dumpファイル修正

---
+++ LoadMaterialTemplateFile.cpp
@@ -0,0 +1,26 @@
+#include "LoadMaterialTemplateFile.h"
+
+/*MaterialData LoadMaterialTemplateFile(const std::string& directoryPath, const std::string& filename) {
+	//変数の宣言
+
+	MaterialData materialData;	//構築するMaterialData
+	std::string line;	//ファイルから読んだ1行を格納するもの
+	std::ifstream file(directoryPath + "/" + filename);	//ファイルを開く
+	assert(file.is_open());	//開けていない場合止める
+
+	//MaterialDataを構築
+	while (std::getline(file, line)) {
+		std::string identifier;
+		std::istringstream s(line);
+		s >> identifier;
+
+		//identifierに応じた処理
+		if (identifier == "map_Kd") {	//map_Kdにはtextureのファイル名が記載されている
+			std::string textureFilename;
+			s >> textureFilename;
+			//連結してファイルパスにする
+			materialData.textureFilePath = directoryPath + "/" + textureFilename;
+		}
+	}
+	return materialData;
+}*/
---
+++ LoadTexture.cpp
@@ -0,0 +1,17 @@
+#include "LoadTexture.h"
+#include "ConvertString.h"
+
+DirectX::ScratchImage LoadTexture(const std::string& filePath) {
+	//テクスチャファイルを読んでプログラムで扱えるようにする
+	DirectX::ScratchImage image{};
+	std::wstring filePathW = ConvertString(filePath);
+	HRESULT hr = DirectX::LoadFromWICFile(filePathW.c_str(), DirectX::WIC_FLAGS_FORCE_SRGB, nullptr, image);
+	assert(SUCCEEDED(hr));
+
+	//ミップマップの作成
+	DirectX::ScratchImage mipImages{};
+	hr = DirectX::GenerateMipMaps(image.GetImages(), image.GetImageCount(), image.GetMetadata(), DirectX::TEX_FILTER_SRGB, 0, mipImages);
+
+	//ミップマップ付きのデータを返す
+	return mipImages;
+}
