557874968a68e1f72c68ddaa64002c865c279a4f
Okuda-Haruto <k024g0057@g.neec.ac.jp>
2025-04-24 04:25:13 +0900

    課題用のImGui操作

--- GetDescriptorHandle.h
+++ GetDescriptorHandle.h
@@ -4,6 +4,10 @@
 #include <wrl.h>
 #include <cstdint>
 
+static uint32_t kLastCPUIndex = 1;
+
+static uint32_t kLastGPUIndex = 1;
+
 D3D12_CPU_DESCRIPTOR_HANDLE GetCPUDescriptorHandle(Microsoft::WRL::ComPtr <ID3D12DescriptorHeap> descriptorHeap, uint32_t descriptorSize, uint32_t index);
 
 D3D12_GPU_DESCRIPTOR_HANDLE GetGPUDescriptorHandle(Microsoft::WRL::ComPtr <ID3D12DescriptorHeap> descriptorHeap, uint32_t descriptorSize, uint32_t index);
--- Object_3D.h
+++ Object_3D.h
@@ -1,22 +1,39 @@
 #pragma once
+
+#include "externals/DirectXTex/DirectXTex.h"
+
 #pragma comment(lib,"d3d12.lib")
 #include <d3d12.h>
+
 #include <wrl.h>
 #include "ModelData.h"
 #include "Material.h"
 #include "TransformationMatrix.h"
+#include "SRT.h"
 
 //3Dオブジェクト
 class Object_3D {
 private:
-	//WVPデータ
-	TransformationMatrix* wvpData_ = nullptr;
 	//ロードしたモデルのデータ
 	ModelData modelData_;
-	//マテリアル用リソース
+	//頂点リソース
+	Microsoft::WRL::ComPtr<ID3D12Resource> vertexResource_;
+	//頂点バッファビュー
+	D3D12_VERTEX_BUFFER_VIEW vertexBufferView_{};
+	//頂点リソースデータ
+	VertexData* vertexData_ = nullptr;
+	//マテリアルリソース
 	Microsoft::WRL::ComPtr<ID3D12Resource> materialResource_;
 	//マテリアルデータ
-	Material* materialData_;
+	Material* materialData_ = nullptr;
+	//WVP用リソース
+	Microsoft::WRL::ComPtr<ID3D12Resource> wvpResource_;
+	//WVPデータ
+	TransformationMatrix* wvpData_ = nullptr;
+	//Transform変数
+	Transform transform{ {1.0f,1.0f,1.0f},
+		{0.0f,0.0f,0.0f},
+		{0.0f,0.0f,0.0f} };
 public:
 	~Object_3D();
 	/// <summary>
@@ -27,5 +44,7 @@ public:
 	/// <param name="device">デバイス</param>
 	void Initialize(const std::string& directoryPath, const std::string& filename, Microsoft::WRL::ComPtr<ID3D12Device>& device);
 
-	void Draw();
+	void Draw(Transform& transform, Vector4 color, Transform& cameraTransform, Microsoft::WRL::ComPtr <ID3D12GraphicsCommandList>& commandList, Microsoft::WRL::ComPtr<ID3D12Resource>& directionalLightResource, D3D12_GPU_DESCRIPTOR_HANDLE& textureSrvHandleGPU);
+
+	ModelData ModelData();
 };
