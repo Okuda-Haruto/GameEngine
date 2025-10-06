16611015f25371fc77699e2e71454164a7d8890c
Okuda-Haruto <k024g0057@g.neec.ac.jp>
2025-10-06 10:54:05 +0900

    BlinnPhangRefrectionModel

--- project/resources/Shader/Object3d.PS.hlsl
+++ project/resources/Shader/Object3d.PS.hlsl
@@ -32,6 +32,54 @@ struct PixelShaderOutput
 Texture2D<float4> gTexture : register(t0);
 SamplerState gSampler : register(s0);
 
+PixelShaderOutput PhangReflectionModel(VertexShaderOutput input, float4 textureColor, float cos)
+{
+    PixelShaderOutput output;
+    float3 diffuse = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
+    if (gMaterial.shininess > 0)
+    {
+        float3 toEye = normalize(gCamera.WorldPosition - input.worldPosition);
+        float3 reflectLight = reflect(gDirectionalLight.direction, normalize(input.normal));
+        float RdotE = dot(reflectLight, toEye);
+            
+        float specularPow = pow(saturate(RdotE), gMaterial.shininess);
+            
+        float3 specular = gDirectionalLight.color.rgb * gDirectionalLight.intensity * specularPow * float3(1.0f, 1.0f, 1.0f);
+        
+        output.color.rgb = diffuse + specular;
+    }
+    else
+    {
+        output.color.rgb = diffuse;
+    }
+    output.color.a = gMaterial.color.a * textureColor.a;
+    return output;
+}
+
+PixelShaderOutput BlinnPhangReflectionModel(VertexShaderOutput input, float4 textureColor, float cos)
+{
+    PixelShaderOutput output;
+    float3 diffuse = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
+    if (gMaterial.shininess > 0)
+    {
+        float3 toEye = normalize(gCamera.WorldPosition - input.worldPosition);
+        float3 halfVector = normalize(-gDirectionalLight.direction + toEye);
+        float NdotH = dot(normalize(input.normal), halfVector);
+            
+        float specularPow = pow(saturate(NdotH), gMaterial.shininess);
+            
+        float3 specular = gDirectionalLight.color.rgb * gDirectionalLight.intensity * specularPow * float3(1.0f, 1.0f, 1.0f);
+        
+        output.color.rgb = diffuse + specular;
+    }
+    else
+    {
+        output.color.rgb = diffuse;
+    }
+    output.color.a = gMaterial.color.a * textureColor.a;
+    return output;
+}
+
 PixelShaderOutput main(VertexShaderOutput input)
 {
     float4 transformedUV = mul(float4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransform);
@@ -49,45 +97,14 @@ PixelShaderOutput main(VertexShaderOutput input)
         float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
         float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
         
-        float3 diffuse = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
-        if (gMaterial.shininess > 0)
-        {
-            float3 toEye = normalize(gCamera.WorldPosition - input.worldPosition);
-            float3 reflectLight = reflect(gDirectionalLight.direction, normalize(input.normal));
-            float RdotE = dot(reflectLight, toEye);
-            float specularPow = pow(saturate(RdotE), gMaterial.shininess);
-            
-            float3 specular = gDirectionalLight.color.rgb * gDirectionalLight.intensity * specularPow * float3(1.0f, 1.0f, 1.0f);
-        
-            output.color.rgb = diffuse + specular;
-        }
-        else
-        {
-            output.color.rgb = diffuse;
-        }
-        output.color.a = gMaterial.color.a * textureColor.a;
+        output = BlinnPhangReflectionModel(input,textureColor, cos);
+
     }
     else if (gMaterial.enableLighting == 1)     //Lambert
     {
         float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
         
-        float3 diffuse = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
-        if (gMaterial.shininess > 0)
-        {
-            float3 toEye = normalize(gCamera.WorldPosition - input.worldPosition);
-            float3 reflectLight = reflect(gDirectionalLight.direction, normalize(input.normal));
-            float RdotE = dot(reflectLight, toEye);
-            float specularPow = pow(saturate(RdotE), gMaterial.shininess);
-            
-            float3 specular = gDirectionalLight.color.rgb * gDirectionalLight.intensity * specularPow * float3(1.0f, 1.0f, 1.0f);
-        
-            output.color.rgb = diffuse + specular;
-        }
-        else
-        {
-            output.color.rgb = diffuse;
-        }
-        output.color.a = gMaterial.color.a * textureColor.a;
+        output = BlinnPhangReflectionModel(input,textureColor, cos);
     }
     else    //none
     {
