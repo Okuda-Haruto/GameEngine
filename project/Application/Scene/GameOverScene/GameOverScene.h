0b91284f7f053a224edae59b417ea5364792b089
Okuda-Haruto <k024g0057@g.neec.ac.jp>
2026-01-06 22:25:50 +0900

    クリア画面とか

---
+++ project/Application/Scene/GameOverScene/GameOverScene.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "BaseScene/BaseScene.h"
+#include "Sprite/Sprite.h"
+#include "BackGround/Skydome/Skydome.h"
+#include "BackGround/Ground/Ground.h"
+#include "BackGround/Fence/Fence.h"
+#include "GameCamera/GameCamera.h"
+#include "DirectionalLight/DirectionalLight.h"
+
+using namespace std;
+
+class GameOverScene : public BaseScene
+{
+private:
+
+	shared_ptr<Input> input_;
+
+	std::unique_ptr<Sprite>fadeSprite_;
+
+	//メインカメラ
+	shared_ptr<Camera> camera_;
+
+	shared_ptr<DirectionalLight> directionalLight_;
+	DirectionalLightElement directionalLightElement_;
+
+	std::unique_ptr<Skydome> skydome_;
+	std::unique_ptr<Ground> ground_;
+	std::unique_ptr<Fence> fence_;
+	std::unique_ptr<Object> player_;
+
+	std::unique_ptr<Sprite>titleSprite_;
+
+	float animationTime = 0.0f;
+
+public:
+
+	~GameOverScene();
+
+	//初期化
+	void Initialize(shared_ptr<Input> input) override;
+	//更新
+	void Update() override;
+	//描画
+	void Draw() override;
+};
+
